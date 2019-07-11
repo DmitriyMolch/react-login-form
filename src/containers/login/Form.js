8903647bffebf2c756d269254eeacc4592510801
Dmitriy Molchanov <dmitriy@decagames.org>
2019-07-11 17:07:31 +0300

    Initial commit.

---
+++ src/containers/login/Form.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "./Form.css";
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        };
+    }
+
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        alert('Form submitted!')
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="email" size="lg">
+                        <FormLabel>Email</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" size="lg">
+                        <FormLabel>Password</FormLabel>
+                        <FormControl
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password"
+                        />
+                    </FormGroup>
+                    <Button
+                        block
+                        size="lg"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                </form>
+            </div>
+        );
+    }
+}
